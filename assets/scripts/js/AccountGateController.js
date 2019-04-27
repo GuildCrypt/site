f0552a293a5f891b3b29423974b77abd9a0f511a
Aakil Fernandes <aakilfernandes@gmail.com>
2019-04-27 13:23:51 -0400

    treat login as async

--- assets/scripts/js/AccountGateController.js
+++ assets/scripts/js/AccountGateController.js
@@ -2,16 +2,17 @@ import { default as accountsManager, NoWeb3Error} from './accountsManager.js'
 
 export default function AccountGateController($scope) {
   $scope.isTosConfirmed = false
-  $scope.login = () => {
+  $scope.login = async () => {
     if (!$scope.isTosConfirmed) {
       return
     }
 
     try {
-      accountsManager.login()
+      await accountsManager.login()
     } catch(error) {
       if (error instanceof NoWeb3Error) {
         $scope.isNoWeb3 = true
+        $scope.$apply()
       }
     }
   }
