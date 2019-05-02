7b40e0a82cfdb23ff831f95cf90af7bd460f0c41
Aakil Fernandes <aakilfernandes@gmail.com>
2019-05-01 22:24:45 -0400

    incorporate feedback and stylieze

--- assets/scripts/js/TradeController.js
+++ assets/scripts/js/TradeController.js
@@ -4,10 +4,18 @@ import oathforgeApiClient from './oathforgeApiClient.js'
 import fetchZocrscope from './fetchZocrscope.js'
 import web3Manager from './web3Manager.js'
 
-export default function TradeController($scope) {
+export default function TradeController($scope, $interval) {
   const params = document.location.hash.substr(1).split('/')
   const idNumber = parseInt(params[1])
   $scope.isZocrscopeLoaded = false
+
+  $scope.isZocrscopeLoaded
+  $scope.progress = 1
+
+  const interval = $interval(() => {
+    $scope.progress = 100 - Math.round((100 - $scope.progress) / 2)
+  }, 100)
+
 
   oathforgeApiClient.fetchToken(idNumber).then(async (token) => {
     const riftpactData = await token.fetchRiftpactData()
@@ -18,24 +26,28 @@ export default function TradeController($scope) {
 
     setTimeout(async () => { // don't block ui
       const Zocrscope = await fetchZocrscope()
-      const zocrscope = new Zocrscope(
-        web3.currentProvider,
-        zocrAddressHexUnprefixed,
-        exchangeAddressHexUnprefixed,
-        erc20ProxyAddressHexUnprefixed,
-        'Piece',
-        'DAI',
-        'Pieces',
-        'DAI',
-        riftpactData.addressHexUnprefixed,
-        quoteAssetAddressHexUnprefixed,
-        0,
-        18
-      )
-      $scope.isZocrscopeLoaded = true
-      $scope.$apply()
-      zocrscope.appendTo$(document.getElementById('zocrscope'))
-    }, 100)
+      $interval.cancel(interval)
+      $scope.progress = 100
+      setTimeout(() => {
+        const zocrscope = new Zocrscope(
+          web3.currentProvider,
+          zocrAddressHexUnprefixed,
+          exchangeAddressHexUnprefixed,
+          erc20ProxyAddressHexUnprefixed,
+          'Piece',
+          'DAI',
+          'Pieces',
+          'DAI',
+          riftpactData.addressHexUnprefixed,
+          quoteAssetAddressHexUnprefixed,
+          0,
+          18
+        )
+        $scope.isZocrscopeLoaded = true
+        $scope.$apply()
+        zocrscope.appendTo$(document.getElementById('zocrscope'))
+      }, 1000)
+    }, 500)
   })
 
   const zocrAddressHexUnprefixed = networkManager.getZocrAddressHexUnprefixed()
